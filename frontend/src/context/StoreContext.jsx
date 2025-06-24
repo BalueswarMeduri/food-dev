da179bce7c746e5f113dc447c39f37cbb893f73d
Balueswar.Meduri <158301758+BalueswarMeduri@users.noreply.github.com>
2025-06-24 09:41:08 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
     const [food_list, setFoodList] = useState([]); // ✅ Use this instead of imported food_list
     const [token, setToken] = useState("");
-    const url = "http://localhost:4000";
+    const url = "https://food-dev-backend-29qp.onrender.com";
 
     const addToCart = async (itemId) => {
         setCartItems((prev) => ({
